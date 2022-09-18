14df53aebe839b38fe428f13ca2cf856d2068c26
edo979 <prezentabilan@gmail.com>
2022-09-18 15:15:26 +0200

    add style to card flex class

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -4,7 +4,7 @@ function Notification({ data: { user, text, time } }) {
       <img src={user.img} alt="user avatar" className="card_img" />
       <div className="card_body | flex">
         <div className="card_text">
-          <span>{user.name}</span>
+          <span>{user.name} </span>
           <span>{text}</span>
         </div>
         <div className="card_info">{time}</div>
