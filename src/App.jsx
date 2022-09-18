7f166e04b3ec83ee5d8a08b1c61224aaeda830f8
edo979 <prezentabilan@gmail.com>
2022-09-18 07:10:23 +0200

    create notification comp, basic state

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Notification from './Notification'
+
+function App() {
+  const [state, setState] = useState({ notifications: 0 })
+
+  return (
+    <>
+      <header className="header | flex">
+        <h1>
+          Notifications <span className="badge">{state.notifications}</span>
+        </h1>
+        <button className="btn btn-text">Mark all as read</button>
+      </header>
+
+      <main>
+        <ul className="notification-list">
+          <Notification />
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default App
